672d5e273dd63e75482a316ee3b4343cee43143f
Parash2021 <91835658+Parash2021@users.noreply.github.com>
2024-07-25 02:12:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
